a57c1190b5e1f0cf72bac2cfacd5919a30bad87b
Dalton Dick <daltondick2010@gmail.com>
2012-06-04 22:53:03 -0500

    Outlined most of the Settings

--- FreezingIronman/Core.cs
+++ FreezingIronman/Core.cs
@@ -8,8 +8,18 @@ namespace FreezingIronman
 {
     class Core
     {
+        public static Settings settings;
+        static List<Video> videos;
+
         static void Main(string[] args)
         {
+            settings = new Settings();
+            videos = FindVideos();
+        }
+
+        static List<Video> FindVideos()
+        {
+            return null;
         }
     }
 }
--- FreezingIronman/Settings.cs
+++ FreezingIronman/Settings.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace FreezingIronman
 {
-    class Settings
+    public class Settings
     {
+        private string _preset;
+        public string Preset
+        {
+            get
+            {
+                return _preset;
+            }
+        }
+
+        private string _logfile;
+        public string Logfile
+        {
+            get
+            {
+                return _logfile;
+            }
+        }
+
+        private string _inputDirectory;
+        public string InputDirectory
+        {
+            get
+            {
+                return _inputDirectory;
+            }
+        }
+
+        private string _outputDirectory;
+        public string OutputDirectory
+        {
+            get
+            {
+                return _outputDirectory;
+            }
+        }
+
+        private bool _loop;
+        public bool Loop
+        {
+            get
+            {
+                return _loop;
+            }
+        }
+
+        private bool _optimize;
+        public bool Optimize
+        {
+            get
+            {
+                return _optimize;
+            }
+        }
+
+
+        public Settings()
+        {
+
+        }
     }
 }
